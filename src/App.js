5d106e7f77d6a5d543d273bc7ab9fcca3dda533b
Mikhail Kushnir <ro5ti5lav@yandex.ru>
2024-05-18 07:51:50 +0400

    thema7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(form);
+    const query = data.get("newsInput");
+
+    const url = "https://content.guardianapis.com/search";
+    const apiKey = "76e8960f-2f67-417b-88b0-05e88b71ee5a";
+
+    fetch(`${url}?q=${query}&api-key=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setArticles(data.response.results);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-content">
+        <div className="container">
+          <h1>Получите самые свежие новости от газеты <br />The Guardian!</h1>
+          <form className="form" onSubmit={handleSubmit}>
+            <label>Введите интересующую тему:</label>
+            <input type="text" name="newsInput" className="newsInput" />
+            <button className="button" type="submit">Получить список статей</button>
+          </form>
+        </div>
+        <div className="sidebar">
+          {articles.map((item) => (
+            <div key={item.id} className="article">
+              <h2>{item.webTitle}</h2>
+              <p>{item.sectionName}</p>
+              <a href={item.webUrl} target="_blank" rel="noopener noreferrer">Читать далее</a>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
